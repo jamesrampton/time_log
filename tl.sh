9c33017c98f4957418ea18de856d925e251bc536
James Rampton <me@jamesrampton.com>
2021-10-09 00:12:01 +0100

    Add basic time logging script

---
+++ tl.sh
@@ -0,0 +1,10 @@
+#!/bin/sh -
+title="$*"
+SECONDS=0
+start_date=`date "+%F %T"`
+echo "${title} started at ${start_date}"
+read -p "Press enter to finish this time session. "
+elapsedseconds=$SECONDS
+# TODO Parse this into something meaningful
+echo "Finished ${title} after ${elapsedseconds} seconds"
+echo "${start_date}: ${title} - ${elapsedseconds} seconds" >> ~/timelog.txt
