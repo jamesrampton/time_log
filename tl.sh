2e016cb43d6d46dea5e5a91186e00851d75cd1eb
James Rampton <me@jamesrampton.com>
2021-10-09 10:17:35 +0100

    Fix some shellcheck errors

--- tl.sh
+++ tl.sh
@@ -1,9 +1,9 @@
-#!/bin/sh -
+#!/bin/bash -
 title="$*"
 SECONDS=0
-start_date=`date "+%F %T"`
+start_date=$(date "+%F %T")
 echo "${title} started at ${start_date}"
-read -p "Press enter to finish this time session. "
+read -pr "Press enter to finish this time session. "
 elapsedseconds=$SECONDS
 # TODO Parse this into something meaningful
 echo "Finished ${title} after ${elapsedseconds} seconds"
