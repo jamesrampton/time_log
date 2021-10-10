66cff478a374ffdafdc95f04f839410a430b8000
James Rampton <me@jamesrampton.com>
2021-10-10 14:22:45 +0100

    Output time in a human-readable format

--- tl.sh
+++ tl.sh
@@ -1,10 +1,18 @@
 #!/bin/bash -
+
+function displaytime {
+  local T=$1
+  local H=$((T/60/60))
+  local M=$((T/60%60))
+  local S=$((T%60))
+  printf '%02d:%02d:%02d\n' $H $M $S
+}
+
 title="$*"
 SECONDS=0
 start_date=$(date "+%F %T")
 echo "${title} started at ${start_date}"
 read -r -p"Press enter to finish this time session. "
 elapsedseconds=$SECONDS
-# TODO Parse this into something meaningful
-echo "Finished ${title} after ${elapsedseconds} seconds"
-echo "${start_date}: ${title} - ${elapsedseconds} seconds" >> ~/timelog.txt
+echo "Finished ${title} after $(displaytime $elapsedseconds)"
+echo "${start_date}: ${title} - $(displaytime $elapsedseconds)" >> ~/timelog.txt
